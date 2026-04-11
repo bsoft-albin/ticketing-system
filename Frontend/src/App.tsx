5aac608a1caa52999caf1e9554c562c9e51fa558
bsoft-albin <albinantony@dbcyelagiri.edu.in>
2026-04-11 19:57:35 +0530

    frontend files added.

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+      return (
+        <div className="container mt-5">
+            <h1 className="text-primary">Bootstrap working </h1>
+            <button className="btn btn-success">Click Me</button>
+        </div>
+    );
+}
+
+export default App
